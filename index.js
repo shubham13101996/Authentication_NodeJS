5c141d177ea81c3c6fed563dc1cab023999b2a46
shubham pathak <121822895+shubham13101996@users.noreply.github.com>
2023-01-24 17:51:36 +0530

    folder structure and run server

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`My server start on this port ${PORT}`);
+});
